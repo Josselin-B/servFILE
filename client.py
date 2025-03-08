badbb9d88f4c55b0688d065eaf6557ba59c9f8f7
jboucher <josselin2808@gmail.com>
2025-03-08 18:46:44 +0100

    ajout du fichier client.py avec la fonction hash associé

---
+++ client.py
@@ -0,0 +1,27 @@
+import hashlib
+
+serv_ip = "0.0.0.0"
+serv_port = "8000"
+
+
+def calculer_hash_fichier(path, algo="sha256", buffer_size=65536):  #path + algo en sha256 et un buffer pour eviter la congestion de gros fichier
+        
+    try:
+        hash_func = hashlib.new(algo) #init de la fonction de hashage
+                
+        with open(path,"rb") as f: #ouverture du fichier à partir du path en rb read only binary 
+            for chunk in iter(lambda: f.read(buffer_size), b""):
+                hash_func.update(chunk)
+
+                return hash_func.hexdigest()
+    
+    except FileNotFoundError:
+        return f"Erreur : fichier '{path}' introuvable."
+    except Exception as e:
+        return f"Erreur : {e}"
+
+
+if __name__ == "__main__":
+    path_fichier = "journeymap.server.solara_dimension~solarademension.config"
+    print(calculer_hash_fichier(path_fichier))
+
