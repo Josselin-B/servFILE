c36cf939457bb4e8dd7474acaeb01363eeaef51f
jboucher <josselin2808@gmail.com>
2025-03-08 17:45:08 +0100

    ajout du fichier start pour demarrer le serveur

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+title Serveur Minecraft Fabric
+echo Démarrage du serveur...
+"C:\Program Files\Eclipse Adoptium\jdk-21.0.6.7-hotspot\bin\java.exe" -Xmx12G -jar C:\mcSERV\fabric-server-mc.1.20.1-loader.0.16.10-launcher.1.0.1.jar
+pause
